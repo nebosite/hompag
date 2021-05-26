645c3b34d785f64ab02246190869e83a8fbb8e16
Eric Jorgensen <ericjorg@thejcrew.net>
2021-05-25 17:44:32 -0700

    Added basic grid layout

--- clientapp/src/views/MainAppPage.tsx
+++ clientapp/src/views/MainAppPage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import { inject, observer } from "mobx-react";
 import styles from './AppStyles.module.css';
 import { AppModel } from "models/AppModel";
-
+import '../../node_modules/react-grid-layout/css/styles.css'
+import '../../node_modules/react-resizable/css/styles.css'
+import GridLayout from 'react-grid-layout';
 
 @inject("appModel")
 @observer
@@ -14,9 +16,19 @@ extends React.Component<{appModel?: AppModel}>
   // render
   // -------------------------------------------------------------------
   render() {
+    const layout = [
+        {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+        {i: 'c', x: 4, y: 0, w: 1, h: 2}
+      ];
+
     return (
       <div className={styles.MainAppPage}>
-          th hompag
+         <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+            <div key="a" style={{background: "white"}}>a</div>
+            <div key="b" style={{background: "white"}}>b</div>
+            <div key="c" style={{background: "white"}}>c</div>
+        </GridLayout>
       </div>
     );
   };
